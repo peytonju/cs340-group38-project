246ef9d7889ca4396c072f29ff4ab656f9ba0e7f
peytonju <peytonju@oregonstate.edu>
2025-08-13 20:56:06 -0700

    database_connection_details.js citations

--- internal/database_connection_details.js
+++ internal/database_connection_details.js
@@ -1,3 +1,23 @@
+
+/**
+ *	------------------ CITATION NOTICE ------------------
+ *	-- If any function does NOT list a citation, then it
+ *	-- was created originally by either Justice Peyton or
+ *	-- Zachary Wilkins-Olson, and in those cases the entire
+ *  -- team (Justice Peyton, Zachary Wilkins-Olson) is to
+ *  -- be credited.
+ *	-----------------------------------------------------
+*/
+
+/**
+ * for connecting to an SQL database.
+ * 
+ * 7/6/2025
+ * The following content of this file was copied from "Activity 1 - Access and Use the CS340 Database"
+ * from Dr. Michael Curry's Introduction to Databases (CS340) course at Oregon State
+ * University, summer 2025.
+ * https://canvas.oregonstate.edu/courses/2007765/assignments/10118864?module_item_id=25664550
+ */
 const STUDENT_NAME = "dud";
 const PASSWORD = "dud";
 
@@ -14,4 +34,3 @@ const pool = mysql.createPool({
 }).promise();
 
 module.exports = pool;
-
